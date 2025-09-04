6ec4d0304f5ced388837cdb82637cea8ceedd477
cyuzuzogermain <c.germain@alustudent.com>
2025-09-04 10:59:36 -0700

    #

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
-# This script takes one argument and checks if it matches "School"
+# This script takes one argument and checks if it matches exactly "School"
 
 input = ARGV[0]
 
-if input =~ /School/
+if input =~ /^School$/
   puts input
 end
