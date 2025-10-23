cdf74dda7d017300393247d0d6fe683d46e21d6a
cyuzuzogermain <c.germain@alustudent.com>
2025-10-23 04:20:18 -0700

    #

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-#!/usr/bin/python3
 """
 Reddit Top Ten Hot Posts Module
 
@@ -13,6 +12,9 @@ Usage:
 
 import requests
 
+"""
+module documentation
+"""
 def top_ten(subreddit):
     """
     Fetches and prints the titles of the first 10 hot posts of a subreddit.
